e4b2b5c792776412f747754024f27e2ff1ed8a5e
ved shah <veds5861@gmail.com>
2025-02-11 14:34:47 +0530

    align fixed in para fovea

--- CFF_PARA_FOVEA.py
+++ CFF_PARA_FOVEA.py
@@ -298,7 +298,7 @@ class CffParaFovea :
         self.cffValue_max.pack(side='right',pady=10 ,padx=10)
 
         self.create_side_buttons()
-        self.cffpara_label.pack(pady=2)
+        self.cffpara_label.pack()
         self.cffValue_min.pack(side='left',pady=10 ,padx=10)
         self.cffValue_max.pack(side='right',pady=10 ,padx=10)
         self.cffValue_frq.place (x=600, y=35)   
