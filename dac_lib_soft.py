5dffa066a3389eefa696ccf82a0d91ef2d1a8ff0
ved shah <veds5861@gmail.com>
2025-03-05 20:24:55 +0530

    controller added

--- dac_lib_soft.py
+++ dac_lib_soft.py
@@ -384,8 +384,8 @@ class mup4728:
             if 0 <= data_in <= 20:
                 # Since the INNER_LED function divides by 1.6, we need to multiply by 1.6 here
                 # to achieve the intended DAC value
-                # dac_val = int((1200/20) * data_in * 1.6)  # Compensate for the 1.6 division
-                dac_val = 1200  # Compensate for the 1.6 division
+                dac_val = int((1200/20) * data_in * 1.6)  # Compensate for the 1.6 division
+                # dac_val = 1280  # Compensate for the 1.6 division
                 str_data = 'INNER_LED_DAC = ' + str(dac_val)
                 self.get_print(str_data)
                 self.INNER_LED(dac_val)
