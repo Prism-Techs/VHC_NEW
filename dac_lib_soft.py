2e73a320fb1dc4d0a14773dab900b94c62498cf5
ved shah <veds5861@gmail.com>
2025-03-07 18:22:46 +0530

    1500

--- dac_lib_soft.py
+++ dac_lib_soft.py
@@ -387,7 +387,7 @@ class mup4728:
                 # Since the INNER_LED function divides by 1.6, we need to multiply by 1.6 here
                 # to achieve the intended DAC value
                 # dac_val = int((1500/20) * data_in * 1.6)  # Compensate for the 1.6 division
-                dac_val = 1280*1.6  # Compensate for the 1.6 division
+                dac_val = 1500*1.6  # Compensate for the 1.6 division
                 str_data = 'INNER_LED_DAC = ' + str(dac_val)
                 self.get_print(str_data)
                 self.INNER_LED(dac_val)
